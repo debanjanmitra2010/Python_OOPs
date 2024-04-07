5076b3ea3672491536fc4823b1f91b7354e86836
Debanjan Mitra <debanjanmitra2010@gmail.com>
2024-04-07 17:56:31 -0400

    Hard_Coding removed + Transformed to n flatmates

--- Project2-Flatmates_Bill/main.py
+++ Project2-Flatmates_Bill/main.py
@@ -22,10 +22,12 @@ class Flatmates:
         self.days_in_house = days_in_house
         self.name = name
 
-    def pays(self, bill, flatmate2):
-        weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
-        to_pay = bill.amount * weight
-        return to_pay
+    def pays(self, bill, other_flatmates):
+        """Calculates payment considering all flatmates.
+        """
+        total_days = sum(flatmate.days_in_house for flatmate in other_flatmates) + self.days_in_house
+        weight = self.days_in_house / total_days
+        return bill.amount * weight
 
 
 class PdfReport:
@@ -37,14 +39,11 @@ class PdfReport:
     def __init__(self, filename):
         self.filename = filename
 
-    def generate(self, flatmate1, flatmate2, bill):
-        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
-        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
-
+    def generate(self, flatmates, bill):
         pdf = FPDF(orientation='P', unit='pt', format='A4')
         pdf.add_page()
 
-        # Add Icon
+        # Add Icon (Make sure you actually have a 'house.png' file)
         pdf.image('house.png', w=30, h=30)
 
         # Insert Title
@@ -52,40 +51,39 @@ class PdfReport:
         pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align='C', ln=1)
 
         # Insert Period label and value
+        pdf.set_font(family='Times', size=10)
+        pdf.cell(w=100, h=20, txt=f"Total Amount: ${the_bill.amount}", border=0, ln=1)
+        pdf.cell(w=100, h=20, txt=f"Total Flatmates: {len(flatmates)}", border=0, ln=1)
+        pdf.cell(w=100, h=20, border=0, ln=1)
+
+        # Insert Period label and value
         pdf.set_font(family='Times', size=12, style='B')
         pdf.cell(w=100, h=20, txt="Period", border=0)
-        pdf.cell(w=120, h=20, txt=bill.period, border=0, ln=1)
+        pdf.cell(w=120, h=20, txt=the_bill.period, border=0, ln=1)
 
-        # Insert name and due amount of the first flatmate
+        # Insert name and due amount for each flatmate
         pdf.set_font(family='Times', size=12)
-        pdf.cell(w=100, h=20, txt=flatmate1.name, border=0)
-        pdf.cell(w=120, h=20, txt=flatmate1_pay, border=0, ln=1)
-
-        # Insert name and due amount of the second flatmate
-        pdf.set_font(family='Times', size=12)
-        pdf.cell(w=100, h=20, txt=flatmate2.name, border=0)
-        pdf.cell(w=120, h=20, txt=flatmate2_pay, border=0, ln=1)
+        for flatmate in flatmates:
+            pdf.cell(w=100, h=20, txt=flatmate.name, border=0)
+            pdf.cell(w=120, h=20, txt=str(f"${round(flatmate.pays(bill, flatmates), 2)}"), border=0, ln=1)
 
         pdf.output(self.filename)
         webbrowser.open(self.filename)
 
 
+# Get user input
 a = float(input("Hey user, enter the bill amount: "))
 p = input("Enter the bill period: ")
-name1 = input("Enter the Name of Flatmate1: ")
-name2 = input("Enter the Name of Flatmate2: ")
-f1_days_in_house = int(input(f"Enter the days {name1} stayed in the Flat: "))
-f2_days_in_house = int(input(f"Enter the days {name2} stayed in the Flat: "))
+num_flatmates = int(input("With how many flatmates do you want to divide the Bill: "))
 
+# Collect flatmate data
+flatmates = []
+for i in range(num_flatmates):
+    name = input(f"Enter the Name of Flatmate {i + 1}: ")
+    days_in_house = int(input(f"Enter the days {name} stayed in the Flat: "))
+    flatmates.append(Flatmates(name, days_in_house))
 
-the_bill = Bill(amount=float(a), period=p)
-
-# Two Flatmate objects
-f1 = Flatmates(name=name1, days_in_house=f1_days_in_house)
-f2 = Flatmates(name=name2, days_in_house=f2_days_in_house)
-
-print(f"{name1} Pays:", f1.pays(the_bill, flatmate2=f2))
-print(f"{name2} Pays:", f2.pays(the_bill, flatmate2=f1))
-
+# Create bill object and generate report
+the_bill = Bill(amount=a, period=p)
 pdf_report = PdfReport(filename=f"{the_bill.period.replace(' ', '_')}_Report.pdf")
-pdf_report.generate(flatmate1=f1, flatmate2=f2, bill=the_bill)
+pdf_report.generate(flatmates, the_bill)
